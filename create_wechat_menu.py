53ca2c397040f235ff0bee1cbc504d2d6ef45990
bigzhu <vermiliondun@gmail.com>
2016-02-08 16:07:13 +0800

    建立菜单的脚本

---
+++ create_wechat_menu.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import wechat_oper
+if __name__ == '__main__':
+    wechat = wechat_oper.getWechat()
+    menu_data = {
+        'button': [
+            {
+                'type': 'view',
+                'name': '油卡冲值',
+                'url': 'http://yinmore.follow.center/app#!/card_recharge'
+            },
+            {
+                'type': 'view',
+                'name': '油卡管理',
+                'url': 'http://yinmore.follow.center/app#!/card_manager'
+            },
+        ]
+    }
+    print wechat.create_menu(menu_data)
