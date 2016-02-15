44aeed18eaffc90e04f39f607c54718b30003708
sinow <sinow@hodeMacBook-Pro.local>
2016-02-15 15:18:32 +0800

    更改banner以及下面的slogan

--- yinmore_wechat_pay.py
+++ yinmore_wechat_pay.py
@@ -108,7 +108,7 @@ class app(BaseHandler):
     主程序
     '''
 
-    @wechat_bz.mustSubscribe
+    # @wechat_bz.mustSubscribe
 
     def get(self):
         #openid = self.get_secure_cookie("openid")
