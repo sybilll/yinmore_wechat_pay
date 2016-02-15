e9717c9282dc0092e4ecc24938f01ce92bd79afa
bigzhu <vermiliondun@gmail.com>
2016-02-15 12:19:05 +0800

    fix error

--- yinmore_wechat_pay.py
+++ yinmore_wechat_pay.py
@@ -57,7 +57,7 @@ class payDone(BaseHandler):
             openid = data['openid']
             out_trade_no = data['out_trade_no']
             cash_fee = data['cash_fee']
-            where = " openid='%s' id=%s" % (openid, out_trade_no)
+            where = " openid='%s' and id=%s" % (openid, out_trade_no)
             count = pg.update('pay', status='payed', wexin_return=json_data, where=where)
             if count != 1:
                 error_info = 'update failure: %s' + json_data
