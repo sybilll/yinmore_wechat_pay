234bddec1d8703e26f69e4ccbfdd4cabbcc4d50f
bigzhu <vermiliondun@gmail.com>
2016-02-23 18:53:07 +0800

    完成解绑的确认

--- spa/src/components/BindList.vue
+++ spa/src/components/BindList.vue
@@ -26,13 +26,41 @@
       <tr v-for="card in cards">
         <td><a href="/#!/CardDetail/{{card.id}}">{{card.name}}</a></td>
         <td><a href="/#!/CardDetail/{{card.id}}">{{card.card_number}}</a></td>
-        <td><a @click="unbind(card)" href="javascript:;"><i class="remove icon"></i></a></td>
+        <td><a @click="showConfirm(card)" href="javascript:;"><i class="remove icon"></i></a></td>
       </tr>
     </tbody>
   </table>
   <div id="card_no_bind_waring" class="ui small modal">
     <i @click="jump" class="close icon"></i>
     <img src="/static/images/warning.png">
+  </div>
+  <div id="confirm" class="ui small test modal transition hidden">
+    <div class="header">
+      是否与该油卡解绑？
+    </div>
+    <div class="content">
+      <table class="ui celled striped unstackable table">
+        <thead>
+          <tr>
+            <th>
+              <i class="user icon"></i>{{remove_card.name}}
+            </th>
+            <th>
+              <i class="payment icon"></i>{{remove_card.card_number}}
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    <div class="actions">
+      <div class="ui negative button">
+        取消
+      </div>
+      <div @click="unbind" class="ui positive right labeled icon button">
+        确认
+        <i class="checkmark icon"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,6 +71,7 @@
   export default {
     data: function () {
       return {
+        remove_card:{}
       }
     },
     computed: {
@@ -62,11 +91,15 @@
       store.actions.queryCards()
     },
     methods: {
+      showConfirm: function (card) {
+        this.remove_card = card
+        $('#confirm') .modal('show')
+      },
       selectCard: function (card) {
         store.actions.setSelectedCard(card)
       },
-      unbind: function (card) {
-        store.actions.unbindCard(card.id)
+      unbind: function () {
+        store.actions.unbindCard(this.remove_card.id)
       },
       jump: function () {
         window.location.hash = '#!/card_manager'
